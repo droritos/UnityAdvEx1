206f62cdd53d76da4d3821b4900c9ff70ac2d341
YonatanAmm <yonatan1922@gmail.com>
2025-01-08 17:27:25 +0200

    Reach Event Added

--- Assets/Scripts/UIManager.cs
+++ Assets/Scripts/UIManager.cs
@@ -5,19 +5,31 @@ public class UIManager : MonoBehaviour
 {
     [Header("References")]
     [SerializeField] Character elfCharacter;
+    [SerializeField] AgentMovement elfMovement;
     [SerializeField] TextMeshProUGUI coinCollectedText;
+    [SerializeField] TextMeshProUGUI destinationText;
 
     private int coins = 0;
     void Start()
     {
-        coinCollectedText.SetText("0");
+        elfMovement.OnAgentReachDestinationActionEvent += ShowDestenationText;
+        //coinCollectedText.SetText("0");
         //UpdateCoinBalance(coins); // First, initialize the coin balance
-        elfCharacter.OnCoinCollected += UpdateCoinBalance;
+        //elfCharacter.OnCoinCollected += UpdateCoinBalance;
     }
 
     private void UpdateCoinBalance(int coinValue)
     {
         coins += coinValue;
         coinCollectedText.SetText(coins.ToString());
+    }
+
+    private void ShowDestenationText()
+    {
+        if (destinationText != null)
+        {
+            destinationText.enabled = true;
+            Debug.Log("yes");
+        }
     }
 }
